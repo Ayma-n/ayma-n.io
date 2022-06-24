ec44aff47b88490910951fcec37bc72579519b95
Ayma-n <aymanbt14@gmail.com>
2022-06-24 17:01:10 +0100

    adjusts homepage styling, adds links to github and linkedin

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import "./Home.css"
 import aymanProfile from "./images/ayman-profile.png"
 import githubLogo from "./images/GitHub-Mark-Light-64px.png"
@@ -11,12 +12,12 @@ export default function Home() {
                 Ayman Benjelloun Touimi
             </div>
             <div id="menu" className="flex flex-row gap-4 text-lg justify-center pt-5">
-                <div id="home-button">home</div>
-                <div id="projects">projects</div>
-                <div id="blog">blog</div>
+                <Link id="home-link" to="/">home</Link>
+                <Link id="projects-link" to="/projects">projects</Link>
+                <Link id="projects-link" to="/blog">blog</Link>
             </div>
             <div id="ayman-profile">
-                <img className="w-20 ml-6 pt-36" src={aymanProfile}></img>
+                <img className="w-20 ml-6 pt-28" src={aymanProfile}></img>
             </div>
             <div id="main-titles" className="font-bold text-xl pt-4 ml-6">
                 <div>hi there!</div>
@@ -26,10 +27,14 @@ export default function Home() {
                 I’m currently a sophomore at Brown University, studying Computer Science.
             </div>
             <div className="flex flex-row ml-6 pt-3 gap-4" id="social-logos">
-                <img id="github-logo" className="w-7" src={githubLogo} />
-                <img id="linkedin-logo" className="w-7" src={linkedinLogo} />
+                <a href="https://github.com/Ayma-n">
+                    <img id="github-logo" className="w-7" src={githubLogo} />
+                </a>
+                <a href="https://www.linkedin.com/in/ayman-benjelloun-touimi-7b3160202/">
+                    <img id="linkedin-logo" className="w-7" src={linkedinLogo} />
+                </a>
             </div>
-            <div className="box-border rounded-lg w-40 h-11 mt-3 cst-bg-white-transparent border-solid border-2 border-white ml-auto mr-auto">
+            <div className="box-border rounded-xl w-40 h-11 mt-7 cst-bg-white-transparent border-solid border-2 border-white ml-auto mr-auto">
                 <div className="text-center align-middle leading-10">
                     about me
                 </div>
