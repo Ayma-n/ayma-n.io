5706112448a27db4c52855d95cfe523ee7770686
Ayma-n <aymanbt14@gmail.com>
2022-06-27 16:08:04 +0100

    main content in home page is now centered dynamically

--- src/Home.tsx
+++ src/Home.tsx
@@ -7,7 +7,7 @@ import linkedinLogo from "./images/linkedin-3-64.png"
 
 export default function Home() {
     return (
-        <div className="h-screen w-screen text-white" id="Home">
+        <div className="h-screen w-screen text-white flex flex-col" id="Home">
             <div id="name-and-menu" className="pt-8 md:w-screen md:flex md:flex-row md:justify-between">
                 <div id="my-name" className="font-bold text-xl ml-6">
                     Ayman Benjelloun Touimi
@@ -18,33 +18,36 @@ export default function Home() {
                     <Link id="projects-link" to="/blog">blog</Link>
                 </div>
             </div>
-            <div id="main-content" className="md:flex md:flex-row-reverse md:justify-center md:mt-40 md:gap-8">
-                <div id="ayman-profile">
-                    <img className="w-20 ml-6 pt-28 md:w-72 md:pt-0" src={aymanProfile}></img>
-                </div>
-                <div id="text-and-socials">
-                    <div id="main-titles" className="font-bold text-xl md:text-2xl pt-4 ml-6">
-                        <div>hi there!</div>
-                        <div className="text-3xl md:text-4xl">my name is Ayman.</div>
+            <div id="main-content-wrapper" className="flex-grow flex justify-center items-center">
+                <div id="main-content" className="md:flex md:flex-row-reverse md:justify-center md:gap-8">
+                    <div id="ayman-profile">
+                        <img className="w-20 ml-6 md:w-72" src={aymanProfile}></img>
                     </div>
-                    <div id="short-presentation" className="ml-6 pt-2 md:w-3/4">
-                        I’m currently a sophomore at Brown University, studying Computer Science.
-                    </div>
-                    <div className="flex flex-row ml-6 pt-3 gap-4" id="social-logos">
-                        <a href="https://github.com/Ayma-n">
-                            <img id="github-logo" className="w-7" src={githubLogo} />
-                        </a>
-                        <a href="https://www.linkedin.com/in/ayman-benjelloun-touimi-7b3160202/">
-                            <img id="linkedin-logo" className="w-7" src={linkedinLogo} />
-                        </a>
-                    </div>
-                    <div className="box-border rounded-xl w-40 h-11 mt-7 cst-bg-white-transparent border-solid border-2 border-white ml-auto mr-auto md:ml-6 md:mt-12">
-                        <div className="text-center align-middle leading-10">
-                            about me
+                    <div id="text-and-socials">
+                        <div id="main-titles" className="font-bold text-xl md:text-2xl ml-6">
+                            <div>hi there!</div>
+                            <div className="text-3xl md:text-4xl">my name is Ayman.</div>
+                        </div>
+                        <div id="short-presentation" className="ml-6 pt-2 md:w-3/4">
+                            I’m currently a sophomore at Brown University, studying Computer Science.
+                        </div>
+                        <div className="flex flex-row ml-6 pt-3 gap-4" id="social-logos">
+                            <a href="https://github.com/Ayma-n">
+                                <img id="github-logo" className="w-7" src={githubLogo} />
+                            </a>
+                            <a href="https://www.linkedin.com/in/ayman-benjelloun-touimi-7b3160202/">
+                                <img id="linkedin-logo" className="w-7" src={linkedinLogo} />
+                            </a>
+                        </div>
+                        <div className="box-border rounded-xl w-40 h-11 mt-7 cst-bg-white-transparent border-solid border-2 border-white ml-auto mr-auto md:ml-6 md:mt-12">
+                            <div className="text-center align-middle leading-10">
+                                about me
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
     )
 }
